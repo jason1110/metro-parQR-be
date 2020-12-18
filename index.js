04ace195aeb3b08b4f10e701cb2081d5dd8b888c
jason1110 <Jason.easterly11@gmail.com>
2020-12-17 17:40:16 -0700

    add env port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors')
 const bodyParser = require('body-parser')
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 const knex = require('knex')
 const config = require('./knexfile').development

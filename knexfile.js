5b0e610cf424d18ecf39078c6086a840a8766bce
jason1110 <Jason.easterly11@gmail.com>
2020-12-17 18:53:20 -0700

    add knex config

--- knexfile.js
+++ knexfile.js
@@ -7,21 +7,21 @@ module.exports = {
     connection: 'postgres:///metro-parQR-db'
   },
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
+  // staging: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // },
 
   production: {
     client: 'pg',
